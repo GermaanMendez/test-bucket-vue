88be92554457b411f788c106fd097a2f8af24762
German Mendez <mendezgerman678@gmail.com>
2024-11-17 18:00:16 -0300

    test

--- .github/scripts/deploy.sh
+++ .github/scripts/deploy.sh
@@ -1,10 +1,10 @@
-cd out
+cd dist || { echo "El directorio 'dist' no existe. Asegúrate de que el proceso de construcción se haya ejecutado correctamente."; exit 1; }
 
-# Sync bundles with strong cache
-aws s3 sync ./common/favicons s3://$S3_ORIGIN_BUCKET/common/favicons --exclude "site.webmanifest" --metadata-directive 'REPLACE' --cache-control max-age=31536000,public,must-revalidate --delete
-aws s3 sync ./common/assets s3://$S3_ORIGIN_BUCKET/common/assets --metadata-directive 'REPLACE' --cache-control max-age=31536000,public,must-revalidate --delete
-aws s3 sync ./_next s3://$S3_ORIGIN_BUCKET/_next --exclude "data/*" --metadata-directive 'REPLACE' --cache-control max-age=31536000,public,immutable --delete
-aws s3 sync ./_next/data s3://$S3_ORIGIN_BUCKET/_next/data --metadata-directive 'REPLACE' --cache-control max-age=3600,must-revalidate --delete
+# Sincroniza los favicons
+aws s3 sync ./ s3://$S3_ORIGIN_BUCKET/ --exclude "*" --include "favicon.ico" --metadata-directive 'REPLACE' --cache-control max-age=31536000,public,must-revalidate --delete
 
-# Sync HTML and other files with no cache
-aws s3 sync ./ s3://$S3_ORIGIN_BUCKET --exclude "common/favicons/*" --exclude "_next/*" --exclude "common/assets/*" --include "common/favicons/site.webmanifest" --metadata-directive 'REPLACE' --cache-control no-cache,no-store,must-revalidate --delete
+# Sincroniza los archivos de assets (CSS, JS)
+aws s3 sync ./assets s3://$S3_ORIGIN_BUCKET/assets --metadata-directive 'REPLACE' --cache-control max-age=31536000,public,must-revalidate --delete
+
+# Sincroniza el archivo HTML (index.html) sin caché
+aws s3 sync ./ s3://$S3_ORIGIN_BUCKET/ --exclude "*" --include "index.html" --metadata-directive 'REPLACE' --cache-control no-cache,no-store,must-revalidate --delete
